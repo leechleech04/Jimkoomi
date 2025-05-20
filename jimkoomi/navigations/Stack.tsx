bb56899226e158bfaf1b0d094772de2129a0e9df
Lee Chanhyeong <lch2023@naver.com>
2025-05-20 10:17:35 +0000

    Refactor navigation

--- jimkoomi/navigations/Stack.tsx
+++ jimkoomi/navigations/Stack.tsx
@@ -7,12 +7,58 @@ import SelectActivityScreen from '../screens/SelectActivityScreen';
 import { RootStackParamList } from '../types';
 import CreateChecklistScreen from '../screens/CreateChecklistScreen';
 import HomeScreen from '../screens/HomeScreen';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ActivityIndicator, View } from 'react-native';
+import { colors } from '../colors';
 
 const MyStack = createNativeStackNavigator<RootStackParamList>();
 
 const Stack = () => {
+  const [initialRouteName, setInitialRouteName] = useState<
+    keyof RootStackParamList | null
+  >(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkFirstLaunch = async () => {
+      try {
+        const hasLaunched = await AsyncStorage.getItem('jimkoomiChecklist');
+        if (hasLaunched) {
+          setInitialRouteName('Home');
+        } else {
+          await AsyncStorage.setItem('hasLaunched', 'true');
+          setInitialRouteName('Start');
+        }
+      } catch (error) {
+        console.error('AsyncStorage 에러:', error);
+        setInitialRouteName('Start');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkFirstLaunch();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: colors.skyBlue,
+        }}
+      >
+        <ActivityIndicator size="large" color={colors.blue} />
+      </View>
+    );
+  }
+
   return (
     <MyStack.Navigator
+      initialRouteName={initialRouteName ?? 'Start'}
       screenOptions={{
         headerShown: false,
         animation: 'fade',
